d3eb40a1f93856cc81c2dffd152e8fc45fd55745
Euranet Plus <104374044+EuranetPlus@users.noreply.github.com>
2026-02-25 20:05:10 +0100

    Add map configuration

---
+++ src/lib/stores/config-map.js
@@ -0,0 +1,210 @@
+import { writable } from 'svelte/store';
+export const mapConfig = writable({
+  "title": "How many EU citizens participate in the ERASMUS programme?",
+  "subtitle": "In 2024, about 1.3 million students and staff from EU countries traveled to another EU country through the Erasmus programme.",
+  "colourSchemeClasses": 5,
+  "data": "country,id,value,extraInfo\nAustria,AT,37.2\nBelgium,BE,43.6\nBulgaria,BG,13.9\nCroatia,HR,22.1\nCyprus,CY,14.4\nCzechia,CZ,40.1\nDenmark,DK,29.0\nEstonia,EE,13.4\nFinland,FI,32.2\nFrance,FR,110.3\nGermany,DE,115.8\nGreece,EL,49.3\nHungary,HU,29.9\nIreland,IE,48.2\nItaly,IT,148.6\nLatvia,LV,11.5\nLithuania,LT,16.9\nLuxembourg,LU,3.7\nMalta,MT,26.6\nNetherlands,NL,46.2\nPoland,PL,65.4\nPortugal,PT,72.6\nRomania,RO,31.3\nSlovakia,SK,16.3\nSlovenia,SI,17.3\nSpain,ES,216.6\nSweden,SE,26.1",
+  "parsedData": [
+    {
+      "country": "Austria",
+      "id": "AT",
+      "value": 37.2,
+      "extraInfo": false
+    },
+    {
+      "country": "Belgium",
+      "id": "BE",
+      "value": 43.6,
+      "extraInfo": false
+    },
+    {
+      "country": "Bulgaria",
+      "id": "BG",
+      "value": 13.9,
+      "extraInfo": false
+    },
+    {
+      "country": "Croatia",
+      "id": "HR",
+      "value": 22.1,
+      "extraInfo": false
+    },
+    {
+      "country": "Cyprus",
+      "id": "CY",
+      "value": 14.4,
+      "extraInfo": false
+    },
+    {
+      "country": "Czechia",
+      "id": "CZ",
+      "value": 40.1,
+      "extraInfo": false
+    },
+    {
+      "country": "Denmark",
+      "id": "DK",
+      "value": 29,
+      "extraInfo": false
+    },
+    {
+      "country": "Estonia",
+      "id": "EE",
+      "value": 13.4,
+      "extraInfo": false
+    },
+    {
+      "country": "Finland",
+      "id": "FI",
+      "value": 32.2,
+      "extraInfo": false
+    },
+    {
+      "country": "France",
+      "id": "FR",
+      "value": 110.3,
+      "extraInfo": false
+    },
+    {
+      "country": "Germany",
+      "id": "DE",
+      "value": 115.8,
+      "extraInfo": false
+    },
+    {
+      "country": "Greece",
+      "id": "EL",
+      "value": 49.3,
+      "extraInfo": false
+    },
+    {
+      "country": "Hungary",
+      "id": "HU",
+      "value": 29.9,
+      "extraInfo": false
+    },
+    {
+      "country": "Ireland",
+      "id": "IE",
+      "value": 48.2,
+      "extraInfo": false
+    },
+    {
+      "country": "Italy",
+      "id": "IT",
+      "value": 148.6,
+      "extraInfo": false
+    },
+    {
+      "country": "Latvia",
+      "id": "LV",
+      "value": 11.5,
+      "extraInfo": false
+    },
+    {
+      "country": "Lithuania",
+      "id": "LT",
+      "value": 16.9,
+      "extraInfo": false
+    },
+    {
+      "country": "Luxembourg",
+      "id": "LU",
+      "value": 3.7,
+      "extraInfo": false
+    },
+    {
+      "country": "Malta",
+      "id": "MT",
+      "value": 26.6,
+      "extraInfo": false
+    },
+    {
+      "country": "Netherlands",
+      "id": "NL",
+      "value": 46.2,
+      "extraInfo": false
+    },
+    {
+      "country": "Poland",
+      "id": "PL",
+      "value": 65.4,
+      "extraInfo": false
+    },
+    {
+      "country": "Portugal",
+      "id": "PT",
+      "value": 72.6,
+      "extraInfo": false
+    },
+    {
+      "country": "Romania",
+      "id": "RO",
+      "value": 31.3,
+      "extraInfo": false
+    },
+    {
+      "country": "Slovakia",
+      "id": "SK",
+      "value": 16.3,
+      "extraInfo": false
+    },
+    {
+      "country": "Slovenia",
+      "id": "SI",
+      "value": 17.3,
+      "extraInfo": false
+    },
+    {
+      "country": "Spain",
+      "id": "ES",
+      "value": 216.6,
+      "extraInfo": false
+    },
+    {
+      "country": "Sweden",
+      "id": "SE",
+      "value": 26.1,
+      "extraInfo": false
+    }
+  ],
+  "datasetType": "values",
+  "datasetUnit": "fullNumbers",
+  "percentRounded": false,
+  "colourSchemeType": "sequential",
+  "colourScheme": "red",
+  "colourSchemeReverse": false,
+  "headlineAvailable": true,
+  "subheadlineAvailable": true,
+  "tooltipAvailable": true,
+  "scaleBarAvailable": true,
+  "overrideScaleValues": false,
+  "legendAvailable": true,
+  "textSourceAvailable": true,
+  "textSourceDescription": "Source",
+  "textSource": "Erasmus (2026)",
+  "textNoteAvailable": true,
+  "textNoteDescription": "Note",
+  "textNote": "The figures show the number of inbound learners or staff which participated in the ERASMUS programme. ",
+  "textDataAccessAvailable": true,
+  "linkDataAccessDescription": "Access the data",
+  "linkDataAccess": "https://erasmus-plus.ec.europa.eu/news/almost-15-million-people-went-on-an-erasmus-mobility-in-2024-according-to-latest-report#:~:text=Last%20year%2C%20Erasmus+%20supported%20more,sport%20coaches%20and%20young%20people",
+  "legend1": "No data available",
+  "legend1Color": "#E0E0E0",
+  "customUnitLabelAvailable": true,
+  "customUnitLabel": "thousand",
+  "tooltipExtraInfoLabel": "Click here",
+  "translate": {
+    "title": "How many EU citizens participate in the ERASMUS programme?",
+    "subtitle": "In 2024, about 1.3 million students and staff from EU countries traveled to another EU country through the Erasmus programme.",
+    "textNoteDescription": "Note",
+    "textNote": "The figures show the number of inbound learners or staff which participated in the ERASMUS programme. ",
+    "textSourceDescription": "Source",
+    "textSource": "Erasmus (2026)",
+    "linkDataAccessDescription": "Access the data",
+    "legend1": "No data available",
+    "tooltipExtraInfoLabel": "Click here"
+  },
+  "clusters": [],
+  "colorScale": null
+});
